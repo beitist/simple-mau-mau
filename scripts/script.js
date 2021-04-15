126fd3508a256d4253622736e083c85ade2b49ac
beitist <sebastian.stuewe@mailbox.org>
2021-04-15 09:53:05 +0200

    init

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+
+
+const neuesSpielButton = document.getElementById("button-neues-spiel");
+const undoButton = document.getElementById("button-undo");
+const bestenlisteButton = document.getElementById("button-bestenliste");
+
