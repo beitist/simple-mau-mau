df4f67a7b2afb91a9b61bdd8016c3a93191819d2
beitist <sebastian.stuewe@mailbox.org>
2021-04-23 22:48:31 +0200

    feat: restart using MVC model

---
+++ scripts/script2.js
@@ -0,0 +1,72 @@
+
+
+// Model
+class Table {
+  constructor() {
+
+  }
+}
+
+class Player {
+  constructor() {
+
+  }
+}
+
+class Opponent {
+  constructor() {
+
+  }
+}
+
+class Pile {
+  constructor() {
+
+  }
+}
+
+
+
+// View
+class View {
+  constructor() {
+}
+}
+
+class PlayerView {
+  constructor() {
+
+  }
+}
+
+class OpponentView {
+  constructor() {
+
+  }
+}
+
+
+// Controller
+class Game {
+  constructor() {
+
+  }
+}
+
+class PlayerMoves {
+  constructor() {
+
+  }
+}
+
+class OpponentMoves {
+  constructor() {
+
+  }
+}
+
+class PileMoves {
+  constructor() {
+
+  }
+}
